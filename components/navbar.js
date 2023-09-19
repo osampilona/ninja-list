34b5c42809f6209c81f281f783b898c855265ef4
osampilona <brzicamartina@gmail.com>
2023-09-19 18:48:45 +0200

    first commit

---
+++ components/navbar.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <Image src="/logo.png" height={77} width={128} />
+      </div>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/ninjas">Ninja Listing</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
